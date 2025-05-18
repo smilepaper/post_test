340624b7bf72bda1fe0d8b06e0344ede31d0e132
smilepaper <81312233+smilepaper@users.noreply.github.com>
2025-05-19 03:15:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return '這是你的 POST API！'
+
 @app.route('/submit', methods=['POST'])
 def submit():
-    data = request.json
-    print("接收到資料：", data)
+    data = request.get_json()
+    print("收到資料：", data)
     return jsonify({
-        "message": "資料收到囉！",
-        "you_sent": data
+        "message": "收到資料囉！",
+        "yourData": data
     })
 
-@app.route('/', methods=['GET'])
-def home():
-    return "這是你的 POST 測試 API！"
+# 👇 這段非常關鍵
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render 會設定 PORT 環境變數
+    app.run(host='0.0.0.0', port=port)
